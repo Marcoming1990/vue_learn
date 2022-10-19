8eb5ab83d6e0b61e1a3c199b745e8e067ceac4be
xiaomingye1or2 <253173968@qq.com>
2022-10-19 18:15:37 +0800

    组件

---
+++ 19_单文件组件/main.js
@@ -0,0 +1,11 @@
+import App from './App.vue'
+
+//npm config set registry https://registry.npm.taobao.org
+
+new Vue({
+    el:'#root', 
+    template:`
+        <App></App>
+    `,
+    components:{App},
+})
