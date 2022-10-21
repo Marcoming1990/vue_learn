ee025416fd8dec0460e1c59c58df26affbf054fd
xiaomingye1or2 <253173968@qq.com>
2022-10-21 11:06:51 +0800

    todolist

--- vue_test/src/components/MyItem.vue
+++ vue_test/src/components/MyItem.vue
@@ -3,10 +3,10 @@
         <label>
             <!--如下代码也能实现功能，但是不太推荐，因为有点违反原则，因为修改了props -->
             <!-- <input type="checkbox" v-mode1="todo.done"/> -->
-            <input type="checkbox" :checked= "todo.done" @change="handleCheck(todo.id)"/>
-            <span>{{todo.title}}</span>
+            <input type="checkbox" />
+            <span>{{}}</span>
         </label>
-        <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+        <button class="btn btn-danger">删除</button>
     </li>
 </template>
 
@@ -16,6 +16,40 @@
     }
 </script>
 
-<style>
-
+<style scoped>
+    /*item*/
+    li {
+        list-style: none;
+        height: 36px;
+        line-height: 36px;
+        padding: 0 5px;
+        border-bottom: 1px solid #ddd;
+    }
+    li label {
+        float: left;
+        cursor: pointer;
+    }
+    li label li input {
+        vertical-align: middle;
+        margin-right: 6px;
+        position: relative;
+        top: -1px;
+    }
+    li button {
+        float: right;
+        display: none;
+        margin-top: 3px;
+    }
+    li:before {
+        content: initial;
+    }
+    li:last-child {
+        border-bottom: none;
+    }
+    li:hover{
+        background: #ddd;
+    }
+    li:hover button{
+        display: block;
+    }
 </style>
